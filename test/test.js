8175c6aec4b1d771037c05f8b2a4891dceb758e8
Zhanwen xu <leon.xu@kii.com>
2017-08-29 18:46:16 +0800

    init

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var request = require('supertest'),
+    should = require('should'),
+    app = require('../index.js')
+
+describe('CI Test App', () => {
+	const agent = request.agent(app);
+	it('should return hello world', (done) => {
+		agent.get('/')
+		.expect(200)
+		.expect('Hello World', done)
+	})
+})
